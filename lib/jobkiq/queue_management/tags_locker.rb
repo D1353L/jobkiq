9b892a7b0f60c8876fb40f9c36b0f32732934be1
D1353L <D1353L@users.noreply.github.com>
2025-11-12 15:57:41 +0300

    Fix tags locking issue

--- lib/jobkiq/queue_management/tags_locker.rb
+++ lib/jobkiq/queue_management/tags_locker.rb
@@ -13,30 +13,24 @@ module Jobkiq
       end
 
       def lock(tags:, job_id:)
-        @redis.pipelined do
-          tags.each do |tag|
-            @redis.set(processing_tag_key(@queue_name, tag), job_id, nx: true, ex: LOCK_EXPIRATION_SEC)
-          end
+        tags.each do |tag|
+          @redis.set(processing_tag_key(@queue_name, tag), job_id, nx: true, ex: LOCK_EXPIRATION_SEC)
         end
       end
 
       def release(tags:, job_id:)
-        @redis.pipelined do
-          tags.each do |tag|
-            key = processing_tag_key(@queue_name, tag)
-            val = @redis.get(key)
+        tags.each do |tag|
+          key = processing_tag_key(@queue_name, tag)
+          val = @redis.get(key)
 
-            @redis.del(key) if val == job_id
-          end
+          @redis.del(key) if val == job_id
         end
       end
 
       def tags_locked?(tags)
         return false if tags.nil? || tags.empty?
 
-        @redis.pipelined do
-          tags.each { |tag| @redis.get(processing_tag_key(@queue_name, tag)) }
-        end.any?
+        tags.any? { |tag| @redis.get(processing_tag_key(@queue_name, tag)) }
       end
     end
   end
